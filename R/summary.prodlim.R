63c342610964851b012b858574bf94493fc0197a
tag <tag@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-18 18:30:28 +0000

    reduce newdata with unique.data.frame and provide warning
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/eventhistory/pkg/prodlim@138 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/summary.prodlim.R
+++ R/summary.prodlim.R
@@ -9,97 +9,105 @@ summary.prodlim <- function(object,
                             percent=FALSE,
                             showTime=TRUE,
                             ...) {
-  # }}}
-# {{{  classify the situation
-  cens.type <- object$cens.type         # uncensored, right or interval censored
-  model <- object$model                 # survival, competing risks or multi-state
-  ## cluster <- object$clustervar          # clustered data?
-  cotype <- object$covariate.type       # no, discrete, continuous or both
-  # }}}
-# {{{  times
-  jump.times <- object$time
-  if (missing(times) && (length(times <- jump.times) > 50)) 
-    times <- quantile(sort(unique(jump.times)))
-  times <- sort(unique(times))
-  if (any(times>max(jump.times)))
-    warning(call.=TRUE,immediate.=TRUE,paste("\n","Time(s) ",paste(times[times>max(jump.times)],collapse=", ")," are beyond the maximal follow-up time ",max(jump.times),"\n"))
-  ntimes <- length(times)
-  # }}}
-# {{{ intervalCensored
-  if (cens.type=="intervalCensored"){
-    ltab <- data.frame(time=paste("(",paste(signif(object$time[1,],2),
-                         signif(object$time[2,],2),
-                         sep="-"),"]",sep=""),
-                       n.risk=signif(object$n.risk,2),
-                       n.event=signif(object$n.event,2),
-                       ##    n.lost=object$n.lost,
-                       surv=object$surv)
-  }
-  else{
     # }}}
-# {{{  covariates
-if (cotype>1){
-      if (missing(newdata) || length(newdata)==0){
-        X <- object$X
-        if (NROW(X)>max.tables){
-          warning(call.=TRUE,immediate.=TRUE,paste("\nLife tables are available for",NROW(X),"different covariate constellations.\nShown are only the first, the median and the last table ...\nto see other tables use arguments `X' and `max.tables'\n"))
-          X <- X[c(1,round(median(1:NROW(X))),NROW(X)),,drop=FALSE]
+    # {{{  classify the situation
+    cens.type <- object$cens.type         # uncensored, right or interval censored
+    model <- object$model                 # survival, competing risks or multi-state
+    ## cluster <- object$clustervar          # clustered data?
+    cotype <- object$covariate.type       # no, discrete, continuous or both
+    # }}}
+    # {{{  times
+    jump.times <- object$time
+    if (missing(times) && (length(times <- jump.times) > 50)) 
+        times <- quantile(sort(unique(jump.times)))
+    times <- sort(unique(times))
+    if (any(times>max(jump.times)))
+        warning(call.=TRUE,immediate.=TRUE,paste("\n","Time(s) ",paste(times[times>max(jump.times)],collapse=", ")," are beyond the maximal follow-up time ",max(jump.times),"\n"))
+    ntimes <- length(times)
+    # }}}
+    # {{{ intervalCensored
+    if (cens.type=="intervalCensored"){
+        ltab <- data.frame(time=paste("(",paste(signif(object$time[1,],2),
+                               signif(object$time[2,],2),
+                               sep="-"),"]",sep=""),
+                           n.risk=signif(object$n.risk,2),
+                           n.event=signif(object$n.event,2),
+                           ##    n.lost=object$n.lost,
+                           surv=object$surv)
+    }
+    else{
+        # }}}
+        # {{{ with covariates
+        if (cotype>1){
+            if (missing(newdata) || length(newdata)==0){
+                X <- object$X
+                if (NROW(X)>max.tables){
+                    warning(call.=TRUE,immediate.=TRUE,paste("\nLife tables are available for",NROW(X),"different covariate constellations.\nShown are only the first, the median and the last table ...\nto see other tables use arguments `X' and `max.tables'\n"))
+                    X <- X[c(1,round(median(1:NROW(X))),NROW(X)),,drop=FALSE]
+                }
+            } else{
+                X <- unique.data.frame(newdata)
+                if (NROW(X) < NROW(newdata))
+                    warning("Returned is only one summary for each unique value in newdata.")
+            }
+        } else {
+            X <- NULL
         }
-      }
-      else X <- newdata
+        if (model=="survival") {
+            stats <- list(c("surv",1),c("se.surv",0))
+            if (!is.null(object$conf.int))
+                stats <- c(stats,list(c("lower",0),c("upper",1)))
+            if (surv==FALSE){
+                object$cuminc <- 1-object$surv
+                object$se.cuminc <- object$se.surv
+                cuminc.upper <- 1-object$lower
+                cuminc.lower <- 1-object$upper
+                object$lower <- cuminc.lower
+                object$upper <- cuminc.upper
+                stats <- list(c("cuminc",0),c("se.cuminc",0))
+                if (!is.null(object$conf.int))
+                    stats <- c(stats,list(c("lower",0),c("upper",1)))
+            }
+        }
+        if (model=="competing.risks"){
+            stats <- list(c("cuminc",0),c("se.cuminc",0))
+            if (!is.null(object$conf.int))
+                stats <- c(stats,list(c("lower",0),c("upper",0)))
+        }
+        ltab <- lifeTab(object=object,
+                        times=times,
+                        newdata=X,
+                        stats=stats,
+                        intervals=intervals,
+                        percent=percent,
+                        showTime=showTime)
+        if (model=="competing.risks" & !missing(cause)){
+            if (is.numeric(cause) && !is.numeric(names(ltab)))
+                cause <- attr(object$model.response,"states")[cause]
+            # found <- match(cause,attr(object$model.response,"states"),nomatch=FALSE))
+            Found <- match(cause,names(ltab))
+            if (all(Found)) ltab <- ltab[Found]
+            else warning("could not find requested causes in attributes of object$mode.response")
+        }
     }
-    else
-      X <- NULL
-    
-    if (model=="survival") {
-      stats <- list(c("surv",1),c("se.surv",0))
-      if (!is.null(object$conf.int))
-        stats <- c(stats,list(c("lower",0),c("upper",1)))
-      if (surv==FALSE){
-        object$cuminc <- 1-object$surv
-        object$se.cuminc <- object$se.surv
-        cuminc.upper <- 1-object$lower
-        cuminc.lower <- 1-object$upper
-        object$lower <- cuminc.lower
-        object$upper <- cuminc.upper
-        stats <- list(c("cuminc",0),c("se.cuminc",0))
-        if (!is.null(object$conf.int))
-          stats <- c(stats,list(c("lower",0),c("upper",1)))
-      }
+    # }}}
+    # {{{ output
+    if (is.list(ltab)) {
+        tab <- lapply(ltab,function(x){
+            if (is.list(x)) {
+                lapply(x,data.frame) }
+            else{
+                data.frame(matrix(x))
+            }
+        })
     }
-    if (model=="competing.risks"){
-      stats <- list(c("cuminc",0),c("se.cuminc",0))
-      if (!is.null(object$conf.int))
-        stats <- c(stats,list(c("lower",0),c("upper",0)))
+    else{
+        tab <- data.frame(ltab)
     }
-    ltab <- lifeTab(object=object,times=times,newdata=X,stats=stats,intervals=intervals,percent=percent,showTime=showTime)
-    if (model=="competing.risks" & !missing(cause)){
-      if (is.numeric(cause) && !is.numeric(names(ltab)))
-        cause <- attr(object$model.response,"states")[cause]
-      # found <- match(cause,attr(object$model.response,"states"),nomatch=FALSE))
-      Found <- match(cause,names(ltab))
-      if (all(Found)) ltab <- ltab[Found]
-      else warning("could not find requested causes in attributes of object$mode.response")
-    }
-  }
-  # }}}
-  # {{{ output
-  if (is.list(ltab)) {
-    tab <- lapply(ltab,function(x){
-      if (is.list(x)) {
-        lapply(x,data.frame) }
-      else{
-        data.frame(matrix(x))
-      }
-    })
-  }
-  else{
-    tab <- data.frame(ltab)
-  }
-  out <- list(table=ltab)
-  out$model <- model
-  out$cotype <- cotype
-  class(out) <- "summary.prodlim"
-  out
-  # }}}
+    out <- list(table=ltab)
+    out$model <- model
+    out$cotype <- cotype
+    class(out) <- "summary.prodlim"
+    out
+    # }}}
 }
