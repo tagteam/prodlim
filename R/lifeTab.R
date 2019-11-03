a0d90fbcc2c24e39d740a8f10da81133be0a2633
Thomas Alexander Gerds <tag@biostat.ku.dk>
2019-11-03 09:41:48 +0100

    new name risk for cuminc

--- R/lifeTab.R
+++ R/lifeTab.R
@@ -1,6 +1,6 @@
 #  These functions extract the number of subjects atRisk and the number of
 #  events at given times from the object and binds it together with 
-#  quantities like survival prob, cuminc, standard errors, etc. which can 
+#  quantities like survival prob, risk/cuminc, standard errors, etc. which can 
 #  simply be evaluated at the requested times.
 
 lifeTab <- function(object,...){
