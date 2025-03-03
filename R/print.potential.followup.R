4b1a194623524da003ad7d0a46631edf2a3ffea6
Thomas Alexander Gerds <tag@biostat.ku.dk>
2025-03-03 12:52:09 +0100

    new function to calculate median fup

---
+++ R/print.potential.followup.R
@@ -0,0 +1,34 @@
+### print.potential.followup.R --- 
+#----------------------------------------------------------------------
+## Author: Thomas Alexander Gerds
+## Created: Mar  3 2025 (08:23) 
+## Version: 
+## Last-Updated: Mar  3 2025 (11:58) 
+##           By: Thomas Alexander Gerds
+##     Update #: 33
+#----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+##' @export 
+print.potential.followup <- function(x,digits=2,na.val="--",...){
+    cat("Median and inter quartile range (IQR) of the potential follow up time based on the reverse Kaplan-Meier method.\n")
+    fmt = paste0("%1.",digits,"f")
+    class(x) = c("data.table","data.frame")
+    sx = copy(x)
+    set(sx,j = "Median (IQR)",value = gsub("NA","--",sprintf(fmt = paste0(fmt," (",fmt,";",fmt,")"),x$median,x$Q3,x$Q1)))
+    psx = copy(sx)
+    psx[,median := NULL]
+    psx[,Q1 := NULL]
+    psx[,Q3 := NULL]
+    print(psx)
+    invisible(sx)
+}
+
+
+######################################################################
+### print.potential.followup.R ends here
